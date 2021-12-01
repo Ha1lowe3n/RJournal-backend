7a59128f30df53a40e2dab29bfe9637a653c1658
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-12-01 16:16:53 +0800

    create user entity, add migration

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert } from 'typeorm';
+import { hash } from 'bcrypt';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    fullName: string;
+
+    @Column()
+    email: string;
+
+    @Column({ select: false })
+    password: string;
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await hash(this.password, 10);
+    }
+}
