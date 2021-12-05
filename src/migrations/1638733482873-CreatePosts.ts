248ac4a8280ec52fe61b699c54b5b057c3b3f9cf
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-12-06 03:46:04 +0800

    add migration for posts

---
+++ src/migrations/1638733482873-CreatePosts.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreatePosts1638733482873 implements MigrationInterface {
+    name = 'CreatePosts1638733482873'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "posts" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "body" character varying NOT NULL, "tags" text NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "posts"`);
+    }
+
+}
